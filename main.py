7c13c412befc80aa045d1850c4deb69380e6914d
kadosh01 <kadosh01@gmail.com>
2021-09-14 11:00:49 +0100

    added logging and fix on fail to upload bug.wq

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from instabot import Bot
 import schedule
 import time
 import random
+import logging
 
 # login
 usr = "terminal__shoes"  # username
-pwd = "mefeK3ws"  # password
+pwd = "Yy302880406"  # password
 # dirs paths
 POSTS_DIR = './PostsToUpload'
 POSTED_DIR = './Posted'
@@ -18,6 +19,7 @@ STORY_DIR = './Story'
 Post_Text_Path = './postText.txt'
 
 def clean_up():
+    logging.info('{}: clean up'.format(datetime.datetime.now()))
     dir = "config"
     # checking whether config folder exists or not
     if os.path.exists(dir):
@@ -37,23 +39,29 @@ def getMultiPics(image_path, images_dir_list):
     return multiPicsList
 
 def random_picture():
+    logging.info('{}: random picture ...'.format(datetime.datetime.now()))
     clean_up()
     bot = Bot()
 
     bot.login(username=usr, password=pwd)
     images_dir_list = os.listdir(POSTS_DIR)
     image_path = random.choice(images_dir_list) # take random image from dir
-
+    logging.info('{}: Gen Image : {}'.format(datetime.datetime.now(), image_path))
     success = False
     while not success:
-        album_list = getMultiPics(image_path, images_dir_list) # get a package of images associated with the selected image
-        album_path = ['{}/{}'.format(POSTS_DIR, pic) for pic in album_list]
-        text = open(Post_Text_Path, 'r').read() # post caption text
-        image_name = album_path.__getitem__(0) # get the first image in list
-        album_path.remove(image_name)
-        success = bot.upload_photo(image_name, caption=text)
-        if not success:
-            os.rename(image_name, '{}/{}'.format(STORY_DIR, os.path.basename(image_name)))
+        try:
+            album_list = getMultiPics(image_path, images_dir_list) # get a package of images associated with the selected image
+            album_path = ['{}/{}'.format(POSTS_DIR, pic) for pic in album_list]
+            text = open(Post_Text_Path, 'r').read() # post caption text
+            image_name = album_path.__getitem__(0) # get the first image in list
+            album_path.remove(image_name)
+            success = bot.upload_photo(image_name, caption=text)
+            logging.info('{}: bot upload success: {}'.format(datetime.datetime.now(), success))
+            if not success:
+                os.rename(image_name, '{}/{}'.format(STORY_DIR, os.path.basename(image_name)))
+        except Exception as e:
+            logging.error('{}: bot upload failed :{}'.format(datetime.datetime.now(), e))
+            random_pictuer()
     # clean up the image we upload and related pack of photos
     remove_me = "{}.REMOVE_ME".format(image_name)
     if os.path.exists(remove_me):
@@ -62,10 +70,16 @@ def random_picture():
     for image in album_list:
         if image_name ==  '{}/{}'.format(POSTS_DIR, image): continue
         os.rename('{}/{}'.format(POSTS_DIR, image), '{}/{}'.format(POSTED_DIR, image))
-    print('{} New image was posted! {}'.format(datetime.datetime.now(),image_name))
+    logging.info('{} New image was posted! {}'.format(datetime.datetime.now(),image_name))
 
 if __name__ == '__main__':
-    schedule.every(10).hours.do(random_picture)
+    print('Run script before timer ? (y/n)')
+    ans = input()
+    if ans == 'y':
+        random_picture()
+    schedule.every(7).hours.do(random_picture)
+    #schedule.every(1).second.do(random_picture)
+
     while True:
         schedule.run_pending()  # waiting for schedule
         time.sleep(1000)  # countdown 1 second
